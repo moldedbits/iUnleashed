93f98dd32d23a1fae85cd956ba3ad26aff489ade
v-i-s-h-a-l <vishalsingh2706@gmail.com>
2017-09-15 19:13:14 +0530

    Adds dummy Categories screen

--- A-Lang/A-Lang/SupportingFiles/AppDelegate.swift
+++ A-Lang/A-Lang/SupportingFiles/AppDelegate.swift
@@ -18,7 +18,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         FirebaseApp.configure()
 
         window = UIWindow(frame: UIScreen.main.bounds)
-        window?.rootViewController = UINavigationController(rootViewController: CategoriesViewController())
+        window?.rootViewController = UINavigationController(rootViewController: CategoriesOverviewViewController())
         window?.makeKeyAndVisible()
         
         return true
