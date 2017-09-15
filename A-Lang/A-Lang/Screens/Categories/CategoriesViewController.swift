41310acb9b1b53663aa99a38fd136b5680a20849
v-i-s-h-a-l <vishalsingh2706@gmail.com>
2017-09-15 17:52:30 +0530

    Adds pods and folder restructuring

---
+++ A-Lang/A-Lang/Screens/Categories/CategoriesViewController.swift
@@ -0,0 +1,32 @@
+//
+//  CategoriesViewController.swift
+//  A-Lang
+//
+//  Created by Vishal Singh on 15/09/17.
+//  Copyright © 2017 moldedbits. All rights reserved.
+//
+
+import UIKit
+
+class CategoriesViewController: UIViewController {
+
+    @IBOutlet weak var categoriesFlowLayout: UICollectionViewFlowLayout!
+    @IBOutlet weak var categoriesCollectionView: UICollectionView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
