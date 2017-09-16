c8c7e4b364b49bebb327ca8ee57d85d68e86702c
Saurabh Gupta <saurabh@moldedbits.com>
2017-09-16 21:56:42 +0530

    app launch screen: WIP

---
+++ A-Lang/A-Lang/Screens/AppLaunch/AppLaunchViewController.swift
@@ -0,0 +1,32 @@
+//
+//  AppLaunchViewController.swift
+//  A-Lang
+//
+//  Created by Saurabh Gupta on 16/09/17.
+//  Copyright © 2017 moldedbits. All rights reserved.
+//
+
+import UIKit
+
+class AppLaunchViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        delay(1.0) {
+            let homeTabBarController = UIStoryboard.homeTabBarController()
+            self.view.window?.rootViewController = homeTabBarController
+        }
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        navigationController?.setNavigationBarHidden(true, animated: false)
+    }
+
+}
